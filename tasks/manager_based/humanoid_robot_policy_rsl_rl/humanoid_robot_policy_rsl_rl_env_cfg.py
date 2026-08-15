1a2a513d1c26152a0061abe24c6c3aedecee9cb4
Issac <issaccheng2008@gmail.com>
2026-08-15 10:30:00 +0800

    modify_phase2 weights

--- tasks/manager_based/humanoid_robot_policy_rsl_rl/humanoid_robot_policy_rsl_rl_env_cfg.py
+++ tasks/manager_based/humanoid_robot_policy_rsl_rl/humanoid_robot_policy_rsl_rl_env_cfg.py
@@ -150,7 +150,7 @@ CROSSING_STATE_UPDATE_INTERVAL_S = 0.02
 step_reward_std_curriculum_end_step=1
 
 STRIDE_BAR_REWARD_START_ITERATION = 0
-REWARD_STEADY_ITERATION = 250
+REWARD_STEADY_ITERATION = 1500
 PPO_STEPS_PER_ITERATION = 24
 # EL05 nominal torque, used only by the copied walking reward term.
 EL05_RATED_TORQUE = 4
@@ -1056,8 +1056,8 @@ class CurriculumCfg:
                     "feet_height_entering_band_reward": 400.0,
                 },
                 "reward_weight_ranges": {
-                    "stepping_wooden_bar_step_reward": (15.0, 15.0),
-                    "following_wooden_bar_step_reward": (24.0, 15.0),
+                    "stepping_wooden_bar_step_reward": (18.0, 18.0),
+                    "following_wooden_bar_step_reward": (30.0, 18.0),
                     "feet_height_entering_band_reward": (400.0, 400.0),
                 },
                 "start_step": (
